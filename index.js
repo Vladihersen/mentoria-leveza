c98d55726b17b3682a3a6e9cde32c35e6d21a759
Vladihersen <63798843+Vladihersen@users.noreply.github.com>
2026-06-05 14:24:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const routes = require('./src/routes');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middlewares
 app.use(express.json());
